22876d1899e4f24f532a22dde7b6e1d09c70f2d1
CQ <18551681083@163.com>
2020-06-04 00:44:40 +0800

    延迟查找 ObjectProvider extends ObjectFactory Jdk8提供的一些新特性

--- thinking-in-spring/dependency-lookup/src/main/java/org/huhu/thinking/in/spring/dependency/lookup/ObjectProviderDemo.java
+++ thinking-in-spring/dependency-lookup/src/main/java/org/huhu/thinking/in/spring/dependency/lookup/ObjectProviderDemo.java
@@ -1,8 +1,10 @@
 package org.huhu.thinking.in.spring.dependency.lookup;
 
+import org.huhu.thinking.in.spring.ioc.overview.domain.User;
 import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
 
 /**
  * 通过 {@link ObjectProvider} 进行依赖查找
@@ -28,9 +30,26 @@ public class ObjectProviderDemo {
 
 		lookupByObjectProvider(applicationContext);
 
+		// 当上下文中不存在, 给出一个默认值
+		lookupIfAvailable(applicationContext);
+
+		// 找出上下文中指定类型的说有Bean
+		lookupByStreamOps(applicationContext);
+
 		// 关闭应用上下文
 		applicationContext.close();
 
+	}
+
+	private static void lookupByStreamOps(AnnotationConfigApplicationContext applicationContext) {
+		ObjectProvider<String> objectProvider = applicationContext.getBeanProvider(String.class);
+		objectProvider.stream().forEach(System.out::println);
+	}
+
+	private static void lookupIfAvailable(AnnotationConfigApplicationContext applicationContext) {
+		ObjectProvider<User> objectProvider = applicationContext.getBeanProvider(User.class);
+		User user = objectProvider.getIfAvailable(User::createUser);
+		System.out.println(user);
 	}
 
 	/**
@@ -39,8 +58,14 @@ public class ObjectProviderDemo {
 	 * @return 字符串常量
 	 */
 	@Bean
+	@Primary
 	public String helloWorld() {
 		return "Hello World";
+	}
+
+	@Bean
+	public String message() {
+		return "message";
 	}
 
 	private static void lookupByObjectProvider(AnnotationConfigApplicationContext applicationContext) {
