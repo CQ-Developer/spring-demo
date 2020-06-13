6409c62edfa8122da9576dd518a4a4673cff5a15
CQ <18551681083@163.com>
2020-06-13 22:42:51 +0800

    通过ResolvableDependency作为依赖来源

---
+++ thinking-in-spring/dependency-source/src/main/java/org/huhu/thinking/in/spring/ioc/dependency/source/ResolvableDependencySourceDemo.java
@@ -0,0 +1,45 @@
+package org.huhu.thinking.in.spring.ioc.dependency.source;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import javax.annotation.PostConstruct;
+
+/**
+ * ResolvableDependency 作为依赖来源
+ *
+ * 只能用于依赖注入, 不能用于依赖查找, 并没有 Bean 的类型来供我们操作
+ * 只能用于类型方面的依赖注入, 不能用于名称方面的依赖注入
+ *
+ * @author huhu
+ */
+public class ResolvableDependencySourceDemo {
+
+	@Autowired
+	private String value;
+
+	@PostConstruct
+	public void init() {
+		System.out.println(value);
+	}
+
+	public static void main(String[] args) {
+
+		// 创建 BeanFactory 容器
+		AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext();
+
+		// 注册 Configuration Class 配置类 -> Spring Bean
+		applicationContext.register(ResolvableDependencySourceDemo.class);
+
+		// 回调
+		applicationContext.addBeanFactoryPostProcessor(beanFactory -> beanFactory.registerResolvableDependency(String.class, "Hello.World"));
+
+		// 启动 Spring 应用上下文
+		applicationContext.refresh();
+
+		// 关闭 Spring 应用上下文
+		applicationContext.close();
+
+	}
+
+}
