7f83eddb0b4b78153e6a76db973f2e558a6e23ea
HuHu <chenq_developer@126.com>
2020-07-08 14:00:58 +0800

    基于PropertySource的spring外部化配置的使用

---
+++ thinking-in-spring/configuration-metadata/src/main/java/org/huhu/thinking/in/spring/configuration/metadata/PropertySourceDemo.java
@@ -0,0 +1,53 @@
+package org.huhu.thinking.in.spring.configuration.metadata;
+
+import org.huhu.thinking.in.spring.ioc.overview.domain.User;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.MapPropertySource;
+import org.springframework.core.env.MutablePropertySources;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 基于 Properties 资源的外部化配置
+ *
+ * @author huhu
+ */
+@PropertySource("classpath:META-INF/users-bean-definitions.properties")
+public class PropertySourceDemo {
+
+	public static void main(String[] args) {
+		AnnotationConfigApplicationContext annotationConfigApplicationContext = new AnnotationConfigApplicationContext();
+
+		Map<String, Object> source = new HashMap<>();
+		source.put("user.name", "chenqiang");
+		MapPropertySource mapPropertySource = new MapPropertySource("first-property-source", source);
+
+		// 扩展 Environment 中的 PropertySources, 该操作必须在 refresh 方法之前完成
+		MutablePropertySources mutablePropertySources = annotationConfigApplicationContext.getEnvironment().getPropertySources();
+		mutablePropertySources.addFirst(mapPropertySource);
+
+		annotationConfigApplicationContext.register(PropertySourceDemo.class);
+
+		annotationConfigApplicationContext.refresh();
+
+		mutablePropertySources.stream().forEach(System.out::println);
+
+		Map<String, User> userBeanMap = annotationConfigApplicationContext.getBeansOfType(User.class);
+		userBeanMap.forEach((k, v) -> System.out.println("当前Bean的名称: " + k + ", 值: ===>>> " + v));
+
+		annotationConfigApplicationContext.close();
+	}
+
+	@Bean
+	public User user(@Value("${user.id}") long id, @Value("${user.name}") String name) {
+		User user = new User();
+		user.setId(id);
+		user.setName(name);
+		return user;
+	}
+
+}
